7adae0373144948f45de1f7fd77ecad851af9fb7
Sampurna Developer <soniakhatun747818@gmail.com>
2025-11-25 18:37:30 +0530

    Features : Added Dark theme

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ toggleBtn.addEventListener("click", () => {
   hamburgerIcon.style.display = isOpen ? "none" : "inline-block";
   closeIcon.style.display  = isOpen ? "inline-block" : "none";
 });
+
+
+
+document.getElementById("dark-btn").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
+const brightTheme = document.getElementById("sun");
+const darkTheme = document.getElementById("moon");
+
+brightTheme.addEventListener("click", () => {
+  document.body.classList.add("Dark");
+})
+
+darkTheme.addEventListener("click", () => {
+  document.body.classList.add("Light");
+})
+
+
+document.getElementById("hamburgerBtn").addEventListener("click", () => {
+  document.querySelector(".left-side").classList.toggle("active");
+});
